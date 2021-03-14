16b72893d7c32b22052e6de5bbb6bbbdde79b151
feddelegrand7 <ihaddaden.fodeil@gmail.com>
2021-03-14 16:05:10 +0100

    adding the possibility to add labels

--- R/paper_progress.R
+++ R/paper_progress.R
@@ -24,11 +24,13 @@
 #' }
 
 
-paper_progress <- function(progression, style = "primary") {
+paper_progress <- function(progression,
+                           label = paste0(progression,"%"),
+                           style = "primary") {
 
   shiny::tags$div(class = "row",
     shiny::tags$div(class = "progress margin-bottom",
-      shiny::tags$div(class = paste0("bar ", style, " w-", progression))
+      shiny::tags$div(class = paste0("bar ", style, " w-", progression), label)
     )
 
   )
