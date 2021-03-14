578a712b609f791180c86e49ad0d993575fe3fe2
feddelegrand7 <ihaddaden.fodeil@gmail.com>
2021-03-14 16:47:07 +0100

    adding paper_card_plain exampl

--- R/paper_card_plain.R
+++ R/paper_card_plain.R
@@ -1,5 +1,36 @@
 
 
+#' Title
+#'
+#' @param title
+#' @param subtitle
+#' @param text
+#' @param ...
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' if (interactive()) {
+#' ui <- fluidPage(
+#'   paperize(),
+#'   paper_card_plain(
+#'     title = "The R Programming language",
+#'     subtitle = tags$a(href = "https://google.com", "click here"),
+#'     text = "R is a programming language and free software environment for statistical
+#'             computing and graphics supported by the R Foundation for
+#'             Statistical Computing. The R language is widely used among
+#'             statisticians and data miners for developing statistical software and data analysis.",
+#'     h1(a(href = "https://google.com", "click here")),
+#'
+#'   )
+#' )
+#'
+#' server <- function(input, output) {}
+#' shinyApp(ui = ui, server = server)
+#' }
+
+
 paper_card_plain <- function(title,
                              subtitle,
                              text,
