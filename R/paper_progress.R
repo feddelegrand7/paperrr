e21a567edd8cab3600fdfd25087863a65824a21b
feddelegrand7 <ihaddaden.fodeil@gmail.com>
2021-03-14 15:58:29 +0100

    example on paper progress

--- R/paper_progress.R
+++ R/paper_progress.R
@@ -1,5 +1,29 @@
 
 
+#' Title
+#'
+#' @param progression
+#' @param style
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' if (interactive()) {
+#' ui <- fluidPage(
+#'   paperize(),
+#'   paper_progress(progression = 50),
+#'   paper_progress(progression = 50, style = "success"),
+#'   paper_progress(progression = 50, style = "danger"),
+#'   paper_progress(progression = 50, style = "secondary")
+#' )
+#'
+#' server <- function(input, output) {}
+#'
+#' shinyApp(ui = ui, server = server)
+#' }
+
+
 paper_progress <- function(progression, style = "primary") {
 
   shiny::tags$div(class = "row",
